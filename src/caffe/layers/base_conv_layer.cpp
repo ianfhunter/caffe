80dc5ba5641cdd1074932fae660257ff37d781cb
Andrew Anderson <aanderso@tcd.ie>
2018-10-17 09:21:25 +0100

    Try to fix

--- src/caffe/layers/base_conv_layer.cpp
+++ src/caffe/layers/base_conv_layer.cpp
@@ -377,4 +377,23 @@ void BaseConvolutionLayer<Dtype>::backward_gpu_bias(Dtype* bias,
 
 INSTANTIATE_CLASS(BaseConvolutionLayer);
 
+#ifndef CPU_ONLY
+
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (half_fp), (half_fp),
+                             PROTO_TYPES);
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (float), (float),
+                             PROTO_TYPES);
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (double), (double),
+                             PROTO_TYPES);
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (uint8_t), (uint8_t),
+                             PROTO_TYPES);
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (uint16_t), (uint16_t),
+                             PROTO_TYPES);
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (uint32_t), (uint32_t),
+                             PROTO_TYPES);
+INSTANTIATE_CLASS_3T_GUARDED(BaseConvolutionLayer, (uint64_t), (uint64_t),
+                             PROTO_TYPES);
+
+#endif //!CPU_ONLY
+
 }  // namespace caffe
